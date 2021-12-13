0b3f537212e9994ceefbd3dea7bdccde867a6d54
Mike <mikekett4@yahoo.com>
2021-12-13 16:23:21 -0700

    commit tree de178d25fd776bee8d51b4f01362c47d53330459

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -6,10 +6,37 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  # end
+
+  def index
+    cheeses = Cheese.all
+    render json: cheeses, except: [:created_at, :updated_at]
+  end
+
+
+
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #     id: cheese.id,
+  #     name: cheese.name,
+  #     price: cheese.price,
+  #     is_best_seller: cheese.is_best_seller
+  #   }
+  # end
+
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      # status: :not_found will produce a 404 status code
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
   end
 
 end
